0f57a02b7515721a7243aad663836203792b8632
ontama154 <onchanvvv@gmail.com>
2022-03-11 12:20:43 +0900

    Initial Commit

---
+++ Shared/PropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapperApp.swift
+//  Shared
+//
+//  Created by tomoyo_kageyama on 2022/03/11.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
